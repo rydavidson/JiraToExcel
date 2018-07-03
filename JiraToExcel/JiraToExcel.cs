9828c7fddee42725c48bb76c54651f9223b0cae8
Ryan Davidson <radavidson@outlook.com>
2018-07-03 14:22:42 -0600

    Init

---
+++ JiraToExcel/JiraToExcel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraToExcel
+{
+    public class JiraToExcel
+    {
+        private string JiraUrl { get; set; }
+        private string JiraToken { get; set; }
+        private string JiraUsername { get; set; }
+        private string JiraPassword { get; set; }
+
+        public JiraToExcel(string jiraUrl)
+        {
+
+        }
+
+        public JiraToExcel(string jiraUrl, string jiraToken)
+        {
+
+        }
+
+        public JiraToExcel(string jiraUrl, string username, string password)
+        {
+
+        }
+
+        public 
+
+    }
+}
--- JiraToExcel/Models/JiraIssue.cs
+++ JiraToExcel/Models/JiraIssue.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JiraToExcel
+namespace JiraToExcel.Models
 {
-    public class Class1
+    class JiraIssue
     {
     }
 }
